0200b0c8287dda07f96ace40184a310ae9a529e2
Saumya Patel <snp050106@gmail.com>
2025-03-26 18:58:27 -0600

    push

--- code/MoodSync/app/src/main/java/com/example/moodsync/MoodHistoryFragment.java
+++ code/MoodSync/app/src/main/java/com/example/moodsync/MoodHistoryFragment.java
@@ -1,5 +1,6 @@
 package com.example.moodsync;
 
+import android.app.AlertDialog;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -7,11 +8,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.NavOptions;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +27,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Date;
@@ -31,20 +38,119 @@ public class MoodHistoryFragment extends Fragment {
     private RecyclerView moodRecyclerView;
     private MoodHistoryAdapter moodHistoryAdapter;
     private List<MoodHistoryItem> moodHistoryItems = new ArrayList<>();
+    private List<MoodHistoryItem> originalMoodHistoryItems = new ArrayList<>();
     private FirebaseFirestore db;
-    private String currentUserId;
     private static final String TAG = "MoodHistoryFragment";
+
+    //Variables used for Filter
+    Button filterButton;
+    Button filterClear;
+    Spinner filterSpinner;
+    ArrayAdapter<String> filterSpinnerAdapter;
+    List<String> filterSpinnerData;
+    String selectedEmotionalState;
+    String keywordEditTextData;
+    private String currentUserId;
+
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = MoodHistoryFragmentBinding.inflate(inflater, container, false);
-        binding.historyButton.setTextColor(getResources().getColor(R.color.green));
-        binding.historyButton.setIconTint(ColorStateList.valueOf(getResources().getColor(R.color.green)));
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        //this is the code that navigates to all parts of the app.
+        binding.homeButton.setOnClickListener(v -> {
+            NavHostFragment.findNavController(MoodHistoryFragment.this)
+                    .navigate(R.id.action_moodHistoryFragment_to_SecondFragment,
+                            null,
+                            new NavOptions.Builder()
+                                    .setEnterAnim(R.anim.slide_in_right)
+                                    .setExitAnim(R.anim.slide_out_left)
+                                    .setPopEnterAnim(R.anim.slide_in_left)
+                                    .setPopExitAnim(R.anim.slide_out_right)
+                                    .build());
+        });
+        binding.addCircleButton.setOnClickListener(v -> {
+            NavHostFragment.findNavController(MoodHistoryFragment.this)
+                    .navigate(R.id.action_moodHistoryFragment_to_addMoodActivityFragment,
+                            null,
+                            new NavOptions.Builder()
+                                    .setEnterAnim(R.anim.slide_in_right)
+                                    .setExitAnim(R.anim.slide_out_left)
+                                    .setPopEnterAnim(R.anim.slide_in_left)
+                                    .setPopExitAnim(R.anim.slide_out_right)
+                                    .build());
+        });
+
+        binding.mapButton.setOnClickListener(v -> {
+            NavHostFragment.findNavController(MoodHistoryFragment.this)
+                    .navigate(R.id.action_moodHistoryFragment_to_mapsActivity,
+                            null,
+                            new NavOptions.Builder()
+                                    .setEnterAnim(R.anim.slide_in_right)
+                                    .setExitAnim(R.anim.slide_out_left)
+                                    .setPopEnterAnim(R.anim.slide_in_left)
+                                    .setPopExitAnim(R.anim.slide_out_right)
+                                    .build());
+        });
+        binding.diaryButton.setOnClickListener(v -> {
+            NavHostFragment.findNavController(MoodHistoryFragment.this)
+                    .navigate(R.id.action_moodHistoryFragment_to_JournalFragment,
+                            null,
+                            new NavOptions.Builder()
+                                    .setEnterAnim(R.anim.slide_in_right)
+                                    .setExitAnim(R.anim.slide_out_left)
+                                    .setPopEnterAnim(R.anim.slide_in_left)
+                                    .setPopExitAnim(R.anim.slide_out_right)
+                                    .build());
+
+
+        });
+        binding.historyButton.setTextColor(getResources().getColor(R.color.green));
+        binding.historyButton.setIconTint(ColorStateList.valueOf(getResources().getColor(R.color.green)));
+        //Set up filterFunction
+        filterButton = view.findViewById(R.id.filterButton);
+        filterSpinner = view.findViewById(R.id.filterSpinner);
+        filterClear = view.findViewById(R.id.filterClearButton);
+        filterSpinner.setEnabled(false);
+        filterClear.setEnabled(false);
+
+        //Adding data to Spinner
+        filterSpinnerData = Arrays.asList("Choose Option", "Most Recent Week", "Emotional State", "Keyword");
+        filterSpinnerAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, filterSpinnerData);
+        filterSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        filterSpinner.setAdapter(filterSpinnerAdapter);
+
+        MyApplication myApp = (MyApplication) requireActivity().getApplicationContext();
+        currentUserId = myApp.getLoggedInUsername();
+
+        //Set up Filter Data
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Saving original data
+                if (originalMoodHistoryItems.size() == 0){
+                    //Only saves original data once
+                    saveOriginalMoodHistory();
+                }
+                //Managing visibility of spinner
+                if (filterSpinner.getVisibility() == View.VISIBLE) {
+                    filterSpinner.setVisibility(View.INVISIBLE);
+                    filterSpinner.setEnabled(false);
+                    filterClear.setVisibility(View.INVISIBLE);
+                    filterClear.setEnabled(false);
+                } else {
+                    filterSpinner.setVisibility(View.VISIBLE);
+                    filterSpinner.setEnabled(true);
+                    filterClear.setVisibility(View.VISIBLE);
+                    filterClear.setEnabled(true);
+                }
+            }
+        });
 
         // Set up RecyclerView
         binding.moodRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -56,22 +162,149 @@ public class MoodHistoryFragment extends Fragment {
         moodHistoryAdapter.setOnItemClickListener(item -> {
             fetchMoodEventAndNavigate(item);
         });
-        
-
-        // Get the current user ID
-        MyApplication myApp = (MyApplication) requireActivity().getApplicationContext();
-        currentUserId = myApp.getLoggedInUsername();
 
         fetchMoodEvents();
 
-        // Handle navigation button clicks
-        handleNavigationButtonClicked(view);
+        //Detecting which option in Spinner used:
+        filterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedFilter = parent.getItemAtPosition(position).toString();
+                if (selectedFilter.equals("Most Recent Week")){
+                    //Automatically sorting
+                    long currentTime = System.currentTimeMillis();
+                    long oneWeekinMilli = 7 * 24 * 60 * 60 * 1000; //One week in ms
+
+                    List<MoodHistoryItem> filteredMoodHistory = new ArrayList<>();
+                    for (MoodHistoryItem moodht: moodHistoryItems) {
+                        if ((currentTime - moodht.getDate().getTime()) <= oneWeekinMilli) {
+                            filteredMoodHistory.add(moodht);
+                        }
+                    }
+                    // Update the current list with the filtered data
+                    moodHistoryItems.clear();
+                    moodHistoryItems.addAll(filteredMoodHistory);
+                    moodHistoryAdapter.notifyDataSetChanged();
+
+                } else if (selectedFilter.equals("Emotional State")) {
+                    //Open Dialog
+                    AlertDialog.Builder emotionalFilterDialog = new AlertDialog.Builder(getContext());
+                    LayoutInflater emotionalFilterInflater = getLayoutInflater(); //Setting up Dialog
+                    View emotionalDialogView = emotionalFilterInflater.inflate(R.layout.dialog_filter_layout, null);
+                    emotionalFilterDialog.setView(emotionalDialogView);
+
+                    List<String> emotionalFilterSpinnerData = Arrays.asList("Choose Option", "Happy", "Sad", "Angry", "Confused", "Surprised", "Ashamed", "Scared", "Disgusted");
+                    ArrayAdapter<String> emotionalFilterSpinnerAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, emotionalFilterSpinnerData);
+                    emotionalFilterSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+                    Spinner emotionalFilterSpinner = emotionalDialogView.findViewById(R.id.dialogFilterSpinner);
+                    emotionalFilterSpinner.setAdapter(emotionalFilterSpinnerAdapter);
+
+                    emotionalFilterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            selectedEmotionalState = parent.getItemAtPosition(position).toString(); //Contains current emotional state
+                        }
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                            //Do nothing
+                            selectedEmotionalState = "";
+                        }
+                    });
+
+                    emotionalFilterDialog.setTitle("Filter by Emotional State")
+                            .setPositiveButton("OK", (dialog, which) -> {
+                                dialog.dismiss();
+
+                                //Filtering based on emotional state
+                                List<MoodHistoryItem> filteredMoodHistory = new ArrayList<>();
+                                for (MoodHistoryItem moodht: moodHistoryItems) {
+                                    if (moodht.getMood().equals(selectedEmotionalState)) {
+                                        filteredMoodHistory.add(moodht);
+                                    } else if (selectedEmotionalState.equals("Choose Option")){
+                                        filteredMoodHistory.add(moodht);
+                                    }
+                                }
+                                // Update the current list with the filtered data
+                                moodHistoryItems.clear();
+                                moodHistoryItems.addAll(filteredMoodHistory);
+                                moodHistoryAdapter.notifyDataSetChanged();
+
+                            })
+                            .setNegativeButton("Cancel", (dialog, which) -> {
+                                dialog.dismiss();
+                            });
+                    AlertDialog tempEmotionalFilterDialog = emotionalFilterDialog.create();
+                    tempEmotionalFilterDialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_rounded_thick);
+                    tempEmotionalFilterDialog.show();
+
+                } else if (selectedFilter.equals("Keyword")) {
+                    //Open Dialog
+                    AlertDialog.Builder keywordFilterDialog = new AlertDialog.Builder(getContext());
+                    LayoutInflater keywordFilterInflater = getLayoutInflater(); //Setting up Dialog
+                    View keywordDialogView  = keywordFilterInflater.inflate(R.layout.dialog_filter_layout_keyword, null);
+                    keywordFilterDialog.setView(keywordDialogView);
+
+                    EditText keywordEditTextDialog = keywordDialogView.findViewById(R.id.keywordEditTextDialog);
+                    keywordEditTextData = "";
+
+                    keywordFilterDialog.setTitle("Filter by Keyword")
+                            .setPositiveButton("OK", (dialog, which) -> {
+                                dialog.dismiss();
+
+                                //Filtering based on keyword
+                                keywordEditTextData = keywordEditTextDialog.getText().toString();
+                                List<MoodHistoryItem> filteredMoodHistory = new ArrayList<>();
+                                for (MoodHistoryItem moodht: moodHistoryItems) {
+                                    if (moodht.getDescription().contains(keywordEditTextData)) {
+                                        filteredMoodHistory.add(moodht);
+                                    } else if (keywordEditTextData.equals("Choose Option")){
+                                        filteredMoodHistory.add(moodht);
+                                    }
+
+
+                                }
+                                // Update the current list with the filtered data
+                                moodHistoryItems.clear();
+                                moodHistoryItems.addAll(filteredMoodHistory);
+                                moodHistoryAdapter.notifyDataSetChanged();
+                            })
+                            .setNegativeButton("Cancel", (dialog, which) -> {
+                                dialog.dismiss();
+                            });
+
+                    AlertDialog tempKeywordFilterDialog = keywordFilterDialog.create();
+                    tempKeywordFilterDialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_rounded_thick);
+                    tempKeywordFilterDialog.show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Do nothing if nothing is selected
+            }
+        });
 
         binding.addButton.setOnClickListener(v ->
                 NavHostFragment.findNavController(MoodHistoryFragment.this)
                         .navigate(R.id.action_moodHistoryFragment_to_SecondFragment)
-
         );
+
+        //Clear Button Functionality (To restore filters)
+        saveOriginalMoodHistory();
+        filterClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (originalMoodHistoryItems.size() == 0){
+                    return;
+                }
+                filterSpinner.setSelection(0);
+                //Restoring Original Data
+                moodHistoryItems.clear();
+                moodHistoryItems.addAll(originalMoodHistoryItems);
+                moodHistoryAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -80,31 +313,8 @@ public class MoodHistoryFragment extends Fragment {
         fetchMoodEvents();
     }
 
-    private void handleNavigationButtonClicked(View view) {
-        view.findViewById(R.id.home_button).setOnClickListener(v -> {
-            NavController navController = Navigation.findNavController(v);
-            navController.navigate(R.id.action_moodHistoryFragment_to_SecondFragment);
-        });
-
-        view.findViewById(R.id.map_button).setOnClickListener(v -> {
-            NavController navController = Navigation.findNavController(v);
-            navController.navigate(R.id.action_moodHistoryFragment_to_mapsActivity);
-        });
-
-        view.findViewById(R.id.add_circle_button).setOnClickListener(v -> {
-            NavController navController = Navigation.findNavController(v);
-            navController.navigate(R.id.action_moodHistoryFragment_to_addMoodActivityFragment);
-        });
-
-        view.findViewById(R.id.diary_button).setOnClickListener(v -> {
-            NavController navController = Navigation.findNavController(v);
-            // Assuming you have an action to navigate to JournalFragment
-            navController.navigate(R.id.action_moodHistoryFragment_to_JournalFragment);
-        });
-
-        view.findViewById(R.id.history_button).setOnClickListener(v -> {
-            // Already on MoodHistoryFragment, do nothing
-        });
+    private void saveOriginalMoodHistory() {
+        originalMoodHistoryItems = new ArrayList<>(moodHistoryItems);
     }
 
     private void fetchMoodEventAndNavigate(MoodHistoryItem selectedItem) {
@@ -115,7 +325,6 @@ public class MoodHistoryFragment extends Fragment {
                     if (task.isSuccessful() && !task.getResult().isEmpty()) {
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             MoodEvent moodEvent = document.toObject(MoodEvent.class);
-                            moodEvent.setDocumentId(document.getId()); // Store the document ID in MoodEvent
                             moodEvent.setId(document.getId()); // Store the document ID in MoodEvent
 
                             Bundle args = new Bundle();
@@ -132,9 +341,8 @@ public class MoodHistoryFragment extends Fragment {
     }
 
     private void fetchMoodEvents() {
-
         db.collection("mood_events")
-                .whereEqualTo("id", currentUserId) // Assuming 'userId' is the field name
+                .whereEqualTo("id" , currentUserId)
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
